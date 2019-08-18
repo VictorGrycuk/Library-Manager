138696df36979cec3f57197b30577c1b0ba236da
Victor Grycuk <victor@grycuk.net>
2019-08-18 16:27:57 -0300

    Add UserManagement base code

---
+++ SourceCode/LibraryManagementCore/LibraryManagementCore/Modules/UserManagement/UserDB.cs
@@ -0,0 +1,26 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+
+namespace LibraryManagementCore.Modules.UserManagement
+{
+    public class UserDB : IDataBase
+    {
+        private readonly LiteDatabase db;
+        private readonly LiteCollection<User> userCollection;
+
+        public UserDB(string connectionString)
+        {
+            db = new LiteDatabase(connectionString);
+            userCollection = db.GetCollection<User>("User");
+        }
+
+        public void Add(object user) => userCollection.Insert(user as User);
+
+        public void Update(object user) => userCollection.Update(user as User);
+
+        public IEnumerable<object> Find(string field, string value) => userCollection.Find(Query.EQ(field, value));
+
+        public void Delete(string id) => userCollection.Delete(id);
+    }
+}
---
+++ SourceCode/LibraryManagementCore/LibraryManagementCore/Modules/UserManagement/UserManagement.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibraryManagementCore.Modules.UserManagement
+{
+    public class UserManagement
+    {
+        private readonly IDataBase userDB;
+
+        public UserManagement(IDataBase dataBase)
+        {
+            userDB = dataBase;
+        }
+
+        public void AddNewUser(User user)
+        {
+            if (CheckIfUserExists(user.UserID))
+                throw new ArgumentException($"The ID { user.UserID } already exists.");
+
+            userDB.Add(user);
+        }
+
+        public void AddNewUser(string name, string lastName, string userId, string password)
+        {
+            var user = new User
+            {
+                Name = name,
+                LastName = lastName,
+                UserID = userId
+            };
+
+            user.SetPassword(password);
+
+            AddNewUser(user);
+        }
+
+        public void Update(User user)
+        {
+            userDB.Update(user);
+        }
+
+        public List<User> FindByName(string name)
+        {
+            return userDB.Find("Name", name).ToList().ConvertAll(x => x as User);
+        }
+
+        public List<User> FindByLastName(string lastName)
+        {
+            return userDB.Find("LastName", lastName).ToList().ConvertAll(x => x as User);
+        }
+
+        public List<User> FindByUserID(string userID)
+        {
+            return userDB.Find("UserID", userID).ToList().ConvertAll(x => x as User);
+        }
+
+        public void Delete(User user)
+        {
+            userDB.Delete(user.ID.ToString());
+        }
+
+        private bool CheckIfUserExists(string userId)
+        {
+            return FindByUserID(userId).Count > 0 ? true : false;
+        }
+    }
+}
