3865925bc2129d0b45108a31586d3df03070b0a9
Victor Grycuk <victor@grycuk.net>
2019-09-08 01:45:22 -0300

    Update User and Book management modules with the new database manager

--- SourceCode/LibraryManagementCore/LibraryManagementCore/Core.cs
+++ SourceCode/LibraryManagementCore/LibraryManagementCore/Core.cs
@@ -6,22 +6,22 @@ namespace LibraryManagementCore
 {
     public class Core
     {
-        private User curentUser;
-        private UserManagementCore userManagement;
+        //private User curentUser;
+        //private UserManagementCore userManagement;
 
-        public Core(string connectionString)
-        {
-            userManagement = new UserManagementCore(connectionString);
-        }
+        //public Core(string connectionString)
+        //{
+        //    userManagement = new UserManagementCore(connectionString);
+        //}
 
-        public void LogIn(string username, string password)
-        {
-            // Check if the given password checks against the stored password.
-            curentUser = userManagement.LogIn(username, password) ?? throw new Exception("Username or Password invalid");
+        //public void LogIn(string username, string password)
+        //{
+        //    // Check if the given password checks against the stored password.
+        //    curentUser = userManagement.LogIn(username, password) ?? throw new Exception("Username or Password invalid");
 
-            // Update the last accessed date time
-            curentUser.LastAccessed = DateTime.Now;
-            userManagement.UpdateUser(curentUser);
-        }
+        //    // Update the last accessed date time
+        //    curentUser.LastAccessed = DateTime.Now;
+        //    userManagement.UpdateUser(curentUser);
+        //}
     }
 }
