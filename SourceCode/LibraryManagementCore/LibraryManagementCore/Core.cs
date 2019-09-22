69a685b73ff9821b568b3be0a5dec5849dc22f54
Victor Grycuk <victor@grycuk.net>
2019-09-22 03:23:01 -0300

    Update Core files

--- SourceCode/LibraryManagementCore/LibraryManagementCore/Core.cs
+++ SourceCode/LibraryManagementCore/LibraryManagementCore/Core.cs
@@ -2,15 +2,18 @@
 using System;
 using Base.Architecture.DatabaseManager;
 using Base.Architecture.UserManagement.Models;
+using Base.Architecture.Logger;
+using Base.Architecture.LoggerManager;
 
 namespace LibraryManagementCore
 {
-    public class Core
+    public class LibraryCore
     {
+        public LoggerManager Logger;
         private User _currentUser;
         private readonly UserManagementCore _userManagement;
 
-        public Core(string connectionString)
+        public LibraryCore(string connectionString)
         {
             var dbManager = new DBManager(connectionString);
             _userManagement = new UserManagementCore(dbManager);
@@ -18,13 +21,23 @@ namespace LibraryManagementCore
 
         public void LogIn(string username, string password)
         {
-            // Check if the given password checks against the stored password.
-            _currentUser = _userManagement.LogIn(username, password) ??
-                           throw new Exception("Username or Password invalid");
+            try
+            {
+                // Check if the given password checks against the stored password.
+                _currentUser = _userManagement.LogIn(username, password) ??
+                               throw new Exception("Username or Password invalid");
 
-            // Update the last accessed date time
-            _currentUser.LastAccessed = DateTime.Now;
-            _userManagement.UpdateUser(_currentUser);
+                // Update the last accessed date time
+                _currentUser.LastAccessed = DateTime.Now;
+                _userManagement.UpdateUser(_currentUser);
+                Logger?.Log(LoggerManager.LogType.Audit, new LogEntry { Username = _currentUser.Username, Message = "Logged in" });
+            }
+            catch (Exception e)
+            {
+                Logger?.Log(LoggerManager.LogType.Audit, new LogEntry { Username = username, Message = "Failed log in" });
+                Logger?.Log(LoggerManager.LogType.Error, e);
+                throw;
+            }
         }
     }
 }
