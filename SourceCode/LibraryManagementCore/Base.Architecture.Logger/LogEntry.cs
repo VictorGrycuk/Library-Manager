e09f7f721a5c2558c2a57acd34b725edec92fe34
Victor Grycuk <victor@grycuk.net>
2019-09-22 03:22:28 -0300

    Add Logger module

---
+++ SourceCode/LibraryManagementCore/Base.Architecture.Logger/LogEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.Architecture.LoggerManager
+{
+    public class LogEntry
+    {
+        public Guid Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Username { get; set; }
+        public string Message { get; set; }
+        public string Source { get; set; }
+        public string StackTrace { get; set; }
+
+        public LogEntry()
+        {
+            Id = Guid.NewGuid();
+            DateTime = DateTime.Now;
+        }
+
+        public LogEntry(Exception exception)
+        {
+            Id = Guid.NewGuid();
+            DateTime = DateTime.Now;
+            Message = exception.Message;
+            Source = exception.Source;
+            StackTrace = exception.StackTrace;
+        }
+    }
+}
---
+++ SourceCode/LibraryManagementCore/Base.Architecture.Logger/LoggerManager.cs
@@ -0,0 +1,33 @@
+﻿using Base.Architecture.DatabaseManager;
+using Base.Architecture.LoggerManager;
+using System;
+using System.Collections.Generic;
+
+namespace Base.Architecture.Logger
+{
+    public class LoggerManager
+    {
+        public enum LogType
+        {
+            Audit,
+            Error
+        }
+
+        private Dictionary<LogType, DBManager> _loggers = new Dictionary<LogType, DBManager>();
+
+        public void AddNewLogger(DBManager dbManager, LogType logType)
+        {
+            _loggers.Add(logType, dbManager);
+        }
+
+        public void Log<TLogEntry>(LogType logType, TLogEntry t) where TLogEntry : class, new()
+        {
+            _loggers[logType].NewTableConnection<TLogEntry>(logType.ToString()).Add(t);
+        }
+
+        public void Log(LogType logType, Exception exception)
+        {
+            _loggers[logType].NewTableConnection<LogEntry>(logType.ToString()).Add(new LogEntry(exception));
+        }
+    }
+}
