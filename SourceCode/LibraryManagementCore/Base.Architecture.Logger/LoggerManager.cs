a5ebcdb923205b6825da813533c6f5c5b89f1898
Victor Grycuk <victor@grycuk.net>
2019-09-22 17:32:25 -0300

    Add error handling, dispose loggers, new methods

--- SourceCode/LibraryManagementCore/Base.Architecture.Logger/LoggerManager.cs
+++ SourceCode/LibraryManagementCore/Base.Architecture.Logger/LoggerManager.cs
@@ -1,33 +1,80 @@
 ﻿using Base.Architecture.DatabaseManager;
-using Base.Architecture.LoggerManager;
 using System;
 using System.Collections.Generic;
 
 namespace Base.Architecture.Logger
 {
-    public class LoggerManager
+    public class LoggerManager : IDisposable
     {
-        public enum LogType
-        {
-            Audit,
-            Error
-        }
-
-        private Dictionary<LogType, DBManager> _loggers = new Dictionary<LogType, DBManager>();
+        private readonly Dictionary<LogType, DBManager> _loggers = new Dictionary<LogType, DBManager>();
 
         public void AddNewLogger(DBManager dbManager, LogType logType)
         {
-            _loggers.Add(logType, dbManager);
+            try
+            {
+                _loggers.Add(logType, dbManager);
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message == "An item with the same key has already been added.")
+                {
+                    throw new ArgumentException($"The LogType '{ logType.ToString() }' already has a logger associated");
+                }
+
+                throw;
+            }
+        }
+
+        public void Remove(LogType logType)
+        {
+            Find(logType).Dispose();
+            _loggers.Remove(logType);
+        }
+
+        public bool Exists(LogType logType)
+        {
+            return _loggers.ContainsKey(logType);
         }
 
         public void Log<TLogEntry>(LogType logType, TLogEntry t) where TLogEntry : class, new()
         {
-            _loggers[logType].NewTableConnection<TLogEntry>(logType.ToString()).Add(t);
+            Find(logType).NewTableConnection<TLogEntry>(logType.ToString()).Add(t);
         }
 
         public void Log(LogType logType, Exception exception)
         {
-            _loggers[logType].NewTableConnection<LogEntry>(logType.ToString()).Add(new LogEntry(exception));
+            Find(logType).NewTableConnection<LogEntry>(logType.ToString()).Add(new LogEntry(exception));
         }
+
+        private DBManager Find(LogType logType)
+        {
+            try
+            {
+                return _loggers[logType];
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message == "The given key was not present in the dictionary.")
+                {
+                    throw new ArgumentException($"There is no logger associated with '{ logType.ToString() }' LogType");
+                }
+
+                throw;
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach (var manager in _loggers)
+            {
+                manager.Value.Dispose();
+            }
+        }
+    }
+
+    public enum LogType
+    {
+        Audit,
+        Error
     }
 }
